dc01342bdbf2bacc795f668b76071945df976470
rahul-sarma <rahul.sarma@hp.com>
2021-01-18 23:53:10 +0530

    database added

--- db/index.js
+++ db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/portfolio', { useNewUrlParser: true })
+    // .connect('mongodb://127.0.0.1:27017/portfolio', { useNewUrlParser: true })
+    .connect('mongodb+srv://rahul:password12345Abcde@portfolio.wuawd.mongodb.net/portfolio?retryWrites=true&w=majority', { useNewUrlParser: true })
+    
     .catch(e => {
         console.error('Connection error', e.message)
     })
