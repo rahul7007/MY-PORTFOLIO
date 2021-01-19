6a785ff78c2b8611f3492a03d87be64f523c8e2a
rahul-sarma <rahul.sarma@hp.com>
2021-01-19 08:21:59 +0530

    Api added

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://portfolio-rahul-sarma.herokuapp.com/',
+    
 })
 
 export const insertProjectData = payload => api.post(`/insert`, payload)
